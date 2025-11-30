859569265d8ca740e4302e02e66a2026b47c0192
MarianaHernandezG <mahernandezgi@unal.edu.co>
2025-11-30 17:57:30 -0500

    update pruebas_colinealidad.R

--- models/SIVIGILA nacional/pruebas_colinealidad.R
+++ models/SIVIGILA nacional/pruebas_colinealidad.R
@@ -1,6 +1,7 @@
 # Este archivo tiene como objetivo hacer pruebas de multicolinealidad para la base de datos con que se ajsutaron los modelos
 
 # Librerías y funciones necesarias
+library(dplyr)
 library(here)
 library(car)
 source(here("utils", "load_data.R"))
@@ -96,6 +97,11 @@ library(car)
 # GVIF para detectar multicolinealidad
 vif_resultados <- vif(modelo)
 vif_resultados
+#------------------------------------------------------------------------------
+# Todas las variables tienen un valor VIF que se mantiene entre 1 y 1.3.
+# Esto indica que, la colinealidad es mínima.
+#------------------------------------------------------------------------------
+
 
 # Rango de la matriz de diseño
 rango_X <- qr(X)$rank
@@ -111,3 +117,86 @@ if (rango_X < columnas_X) {
 } else {
   print("✔️ No hay multicolinealidad perfecta en la matriz del modelo.")
 }
+
+
+
+
+
+# Prueba t para comparar edades entre los grupos de atención en salud mental
+
+t.test(edad_ ~ ac_mental, data = data_model_SIVIGILA)
+#------------------------------------------------------------------------------
+# H0: mediaNO = mediaSI
+# H1: mediaNO diferente mediaSI
+
+# - media grupo 0: 27.86 años
+# - media grupo 1: 26.10 años
+# - diferencia ~ 1.76 años
+# - vp < 2e-16: estadísticamente significativa
+
+# La magnitud de la diferencia es muy pequeña: solo 1.7 años de diferencia entre 
+# grupos, aunque resulte estadísticamente significativa (puede ser por la muestra grande)
+#------------------------------------------------------------------------------
+
+
+# Modelo logístico univariado para la edad
+m_uni <- glm(ac_mental ~ edad_, data = data_model_SIVIGILA, family = binomial)
+sum_m_uni <- summary(m_uni)
+
+# Odds ratio del efecto de edad_
+exp(sum_m_uni$coefficients[2])
+#------------------------------------------------------------------------------
+# El odds ratio para edad_ en el modelo univariado es de 0.9812 aprox. 
+# Esto indica que, cada que la edad aumenta en un año, la probabilidad de 
+# recibir atención en salud mental cambia menos del 1%.
+
+
+# Aunque, el parámetro da estadísticamente significativo, esto indica que el efecto 
+# de la edad sobre la variable respuesta tiene un efecto pero es muy pequeño (-0.018)
+#------------------------------------------------------------------------------
+
+
+
+# Modelos con y sin la variable edad_
+m_con <- glm(ac_mental ~ edad_ + mujer_cabf + def_naturaleza + area +
+               sexo_agre + conv_agre + pac_hos + escenario,
+             data = data_model_SIVIGILA, family = binomial)
+
+m_sin <- glm(ac_mental ~ mujer_cabf + def_naturaleza + area +
+               sexo_agre + conv_agre + pac_hos + escenario,
+             data = data_model_SIVIGILA, family = binomial)
+
+
+library(pROC)
+
+roc_con <- roc(data_model_SIVIGILA$ac_mental, fitted(m_con))
+roc_sin <- roc(data_model_SIVIGILA$ac_mental, fitted(m_sin))
+
+auc(roc_con)
+auc(roc_sin)
+#------------------------------------------------------------------------------
+# Los valores de AUC para los modelos con y sin la variable edad_ son casi idénticos.
+# Esto significa que incluir edad_ no mejora la capacidad predictiva del modelo.
+#
+# La diferencia en AUC es < 0.01, lo cual se considera irrelevante.
+# Esto refuerza la conclusión de que edad_ no aporta valor predictivo al modelo.
+#------------------------------------------------------------------------------
+
+
+
+
+# Distribución de la edad según la variable respuesta
+library(ggplot2)
+ggplot(data_model_SIVIGILA, aes(x = factor(ac_mental),
+                                y = edad_,
+                                fill = factor(ac_mental))) +
+  geom_violin(alpha = 0.3) +
+  geom_boxplot(width = 0.2, outlier.alpha = 0) +
+  labs(title = "Distribución de edad según atención",
+       x = "Atención en salud mental (0 = No, 1 = Sí)",
+       y = "Edad") +
+  theme_minimal() +
+  guides(fill = "none")
+
+
+
